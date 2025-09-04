5a3b5c08999c435e4aacedd54b8bf63b874ae4be
Pedro Henrique <pedro.progsantos@gmail.com>
2025-09-03 23:58:19 -0300

    Validação de inicio de jogo

--- src/pages/jogo/[portas]/[temPresente].tsx
+++ src/pages/jogo/[portas]/[temPresente].tsx
@@ -10,12 +10,21 @@ export default function Jogo() {
 
   const router = useRouter();
   const [portas, setPortas] = useState([]);
+  const [valido, setValido] = useState(false);
 
   useEffect(() => {
     const portas = +router.query.portas;
     const temPresente = +router.query.temPresente;
     setPortas(criarPortas(portas, temPresente));
   }, [router?.query]);
+
+  useEffect(() => {
+    const portas = +router.query.portas;
+    const temPresente = +router.query.temPresente;
+    const qtdePortaValidas = portas >= 3 && portas;
+    const temPresenteValido = temPresente >= 1 && temPresente <= portas;
+    setValido(qtdePortaValidas && temPresenteValido);
+  }, [portas]);
 
   const renderizarPortas = () => {
     return portas.map(porta => (
@@ -29,7 +38,7 @@ export default function Jogo() {
   return (
     <div className={styles.jogo}>
       <div className={styles.portas}>
-        {renderizarPortas()}
+        {valido ? renderizarPortas() : <h2>Valores invalidos</h2>}
       </div>
 
       <div className={styles.botoes}>
